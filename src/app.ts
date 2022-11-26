5468cdfaced22273a6f2ebbec43d6ae4c43661ae
ABiteOfSorrow <laceican@naver.com>
2022-11-26 21:11:25 +0100

    project setting

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import classA, { Hello, User } from './type';
+
+const helloMessage: Hello = {
+    text: 'hello world'
+}
+
+new classA().a();
---
+++ src/type.ts
@@ -0,0 +1,14 @@
+export interface Hello {
+    text: string;
+}
+
+export const User = {
+    name: 'user1'
+}
+
+// if use export default, you can export only default type for this file.
+export default class A {
+    a() {
+        
+    }
+}
