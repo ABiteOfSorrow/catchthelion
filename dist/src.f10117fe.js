5fd28f2674143940af7a1f7dbe1fe599538de822
ABiteOfSorrow <laceican@naver.com>
2022-11-30 16:02:16 +0100

    add readme.md

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -621,7 +621,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5173" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3652" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+```bash
+npm run dev
+
+http://localhost:1234 
+```
+
+![title](https://res.cloudinary.com/rupo/image/upload/v1669820422/catchthelion_twxlcr.png?raw=true)
